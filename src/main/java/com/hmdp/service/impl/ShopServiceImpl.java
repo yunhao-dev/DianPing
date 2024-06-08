87c5481ad195bf1115f6ba01617edc5855a6960c
23849 <2384982531@qq.com>
2024-06-09 05:11:07 +0800

    2024-06-09-4:59_yunhao-dev 解决缓存穿透

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -38,11 +38,21 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
             Shop shop = JSONUtil.toBean(shopJson, Shop.class);
             return Result.ok(shop);
         }
-
+        //判断命中的是否是空值
+        if(null != shopJson){
+            return Result.fail("店铺信息不存在");
+        }
         // 4.不存在，根据id查询数据库
         Shop shop = getById(id);
         // 5.不存在，返回错误
         if(null == shop){
+            // 将空值写入Redis
+            stringRedisTemplate.opsForValue().set(
+                    RedisConstants.CACHE_SHOP_KEY+id,
+                    "",
+                      RedisConstants.CACHE_NULL_TTL,
+                      TimeUnit.MINUTES);
+            //返回错误信息
             return Result.fail("店铺不存在");
         }
         // 6.存在，写入数据库
